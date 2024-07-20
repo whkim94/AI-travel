a229b6eb0722cbe533eee8d18df317369bf10f55
Jonathan Kim <whkim94@gmail.com>
2024-07-20 00:35:58 -0700

    Fix: Herosection text color

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,35 +1,24 @@
 import React from 'react';
 
-import { Box, useTheme, Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 
-import { getContrastColor } from 'src/utils/colorUtils';
-
-function HeroSection() {
-  const theme = useTheme();
-  const backgroundColor = theme.palette.background.paper;
-  const textColor = getContrastColor(backgroundColor);
-
-  return (
-    <Box
-      sx={{
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '25vh',
-        display: 'flex',
-        alignItems: 'center',
-        color: 'white',
-      }}
-    >
-      <Container>
-        <Typography variant="h2" component="h1" color="textColor" gutterBottom>
-          MoodTrek
-        </Typography>
-        <Typography variant="h5" color="textColor">
-          Discover your perfect itinerary based on your mood
-        </Typography>
-      </Container>
-    </Box>
-  );
-}
+const HeroSection: React.FC = () => (
+  <Box
+    sx={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '25vh',
+      display: 'flex',
+      alignItems: 'center',
+    }}
+  >
+    <Container>
+      <Typography variant="h2" component="h1" gutterBottom>
+        MoodTrek
+      </Typography>
+      <Typography variant="h5">Discover your perfect itinerary based on your mood</Typography>
+    </Container>
+  </Box>
+);
 
 export default HeroSection;
